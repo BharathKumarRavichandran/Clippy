6606dd3eed3637cd01d7c40888f2135d0edd877a
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-06-05 16:59:32 +0530

    Added function to remove elements easily using .remove()

--- functions.js
+++ functions.js
@@ -1,3 +1,17 @@
+Element.prototype.remove = function() {  //Function written to remove element easily using .remove()
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+
+function home(){
+	window.location = "menu.php";
+}
 
 function notes(){
 	window.location = "notes.php";
