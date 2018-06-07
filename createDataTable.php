b33977f465de24ca8dafa37613eed13c0ae47f53
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-06-07 17:39:20 +0530

    New Edit time and create time columns added

--- createDataTable.php
+++ createDataTable.php
@@ -13,6 +13,8 @@ $sql = "CREATE TABLE IF NOT EXISTS $tableName1(
 		Checked VARCHAR(200) NOT NULL,
 		TaskText VARCHAR(8000) NOT NULL,
 		Starred VARCHAR(200) NOT NULL,
+		EditTime VARCHAR(1000) NOT NULL,
+		CreateTime VARCHAR(1000) NOT NULL,
 		PRIMARY KEY (id,TaskNumber)
 		)";
 $conn->query($sql);
@@ -27,6 +29,8 @@ $sql = "CREATE TABLE IF NOT EXISTS $tableName2(
 		NoteNumber INT(100) NOT NULL,
 		Title VARCHAR(200) NOT NULL,
 		NoteText VARCHAR(8000) NOT NULL,
+		EditTime VARCHAR(1000) NOT NULL,
+		CreateTime VARCHAR(1000) NOT NULL,
 		PRIMARY KEY (id,NoteNumber)
 		)";
 $conn->query($sql);
