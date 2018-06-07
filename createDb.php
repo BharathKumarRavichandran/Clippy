88a7d56693ccfb9e64c5d376835c253b52ce1ff3
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-06-07 17:38:07 +0530

    New count columns added

--- createDb.php
+++ createDb.php
@@ -11,6 +11,8 @@ $sql = "CREATE TABLE IF NOT EXISTS user(
 		username VARCHAR(100) NOT NULL,
 		email VARCHAR(320) NOT NULL,
 		password VARCHAR(128) NOT NULL,
+		noteMaxCount INT(100) NOT NULL DEFAULT '0',
+		taskMaxCount INT(100) NOT NULL DEFAULT '0',
 		PRIMARY KEY (id,username)
 		)";
 $conn->query($sql);
