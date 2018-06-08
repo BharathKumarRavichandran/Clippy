9830692d3f4bc74fe93b4af4a897af53a6587bfa
BharathKumarRavichandran <BharathKumarRavichandran@gmail.com>
2018-06-08 22:03:06 +0530

    login page styled

--- login.php
+++ login.php
@@ -28,12 +28,12 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 				header("location: menu.php");  
 			}
 			else{
-				$_SESSION['message'] = "*Sorry, Wrong Password!";
+				$_SESSION['message'] = "Sorry, Wrong Password!";
 			}
 		}
 	}	
 	else{
-		$_SESSION['message'] = "*Username doesn't exist!";
+		$_SESSION['message'] = "Username doesn't exist!";
 	}	
 }
 
@@ -44,17 +44,119 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 <head>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<title>Login | Clippy</title>
+	<link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
 	<link rel="icon" type="image/png" href="assets/webnote.png">
-	<link rel="stylesheet" type="text/css" href="login.css">
+	<style type="text/css">
+		
+	html,body{
+			margin: 0;
+			padding: 0;
+			background: #1A1A1D;
+			font-family: 'Sofia';
+		}
+
+		.title{
+			text-align: center;
+			background: #111111;
+			color: yellow;
+			font-family: 'Sofia';
+			letter-spacing: 0.4em;
+			font-size: 4em;
+			padding: 5px;
+		}
+
+		.tagline{
+			text-align: center;
+			color: yellow;
+			font-family: 'Sofia';
+			letter-spacing: 0.2em;
+			font-size: 2em;
+			padding: 5px;
+			margin-bottom: 2%;
+		}
+
+		#loginTitle{
+			text-align: center;
+			color: orange;
+			font-family: 'Sofia';
+			letter-spacing: 0.4em;
+			font-size: 2.5em;
+			padding: 5px;
+			padding-bottom: 0px;
+		}
+
+		#errMsg{
+			background: red;
+			margin-top: -10px;
+			letter-spacing: 0.1em;
+			font-size: 1.1em;
+			margin-left: 43.2vw;
+			width: 14vw;
+			padding: 3px;
+			border-radius: 4px;
+		}
+
+		#usernameIn{
+			height: 20px;
+			border-radius: 3px;
+			margin: 10px;
+			font-family: 'Trubuchet MS';
+			letter-spacing: 0.1em;
+			font-size: 1.3em;
+			margin-left: 42.6vw;
+		}
+
+		#passIn{
+			height: 20px;
+			border-radius: 3px;
+			margin: 10px;
+			font-family: 'Trubuchet MS';
+			letter-spacing: 0.1em;
+			font-size: 1.3em;
+			margin-left: 42.6vw;
+		}
+
+		#submitIn{
+			margin-top: 1%;
+			border-radius: 3px;
+			font-family: 'Sofia';
+			letter-spacing: 0.3em;
+			font-size: 1.1em;
+			margin-left: 46.6vw;
+			width: 7vw;
+		}
+
+		#foot{
+			margin-top:26vh;
+			color: white;
+			text-align: center;
+			font-size: 1.3em;
+			letter-spacing: 4px;
+		}
+
+		#heart{
+			color: red;
+		}
+
+		#nameLink{
+			text-decoration: none;
+			color: orange;
+		}
+
+	</style>
 </head>
 <body>
-	<h1>Clippy</h1>
-    <h2>Login</h2>
-    <form action="login.php" method="post" autocomplete="off">
-    	<div><?= $_SESSION['message'] ?></div>
-    	<input type="text" placeholder="Username" name="username" required />
-	    <input type="password" placeholder="Password" name="password" autocomplete="new-password" required />
-	    <input type="submit" value="Login" name="login"/>
+	<a onclick="#"><h1 class="title">Clippy</h1></a>
+    <div class="tagline"> A web app to store notes and to-do's</div>
+    <h2 id="loginTitle">Login</h2>
+	<form class="formClass" action="login.php" method="post" autocomplete="off">
+	    <div id="errMsg"><?= $_SESSION['message'] ?></div>
+	    <div><input id="usernameIn" type="text" placeholder="Username" name="username" required /></div>
+		<div><input id="passIn" type="password" placeholder="Password" name="password" autocomplete="new-password" required /></div>
+	    <div><input id="submitIn" type="submit" value="Login" name="login"/></div>
     </form>
+    <footer>
+		<p id="foot">Made with <span id="heart">&hearts;</span> by <a id="nameLink" href="https://BharathKumarRavichandran.github.io">Bharath Kumar Ravichandran</a></p>
+	</footer>
 </body>
 </html>
